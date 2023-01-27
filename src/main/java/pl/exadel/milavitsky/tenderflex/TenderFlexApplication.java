a548362023aa4f5f369f8be4e07656b0fa9b4a3d
Stanislav <stanislav.milavitsky@gmail.com>
2023-01-27 23:03:44 +0100

    first commit

---
+++ src/main/java/pl/exadel/milavitsky/tenderflex/TenderFlexApplication.java
@@ -0,0 +1,13 @@
+package pl.exadel.milavitsky.tenderflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderFlexApplication.class, args);
+	}
+
+}
